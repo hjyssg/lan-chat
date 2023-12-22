f0fe81131ec2cda96c1e3a9e091d128703216c0c
hjy <the398355565@gmail.com>
2023-12-22 14:48:07 +0800

    add date

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret_key'
@@ -13,7 +14,9 @@ def index():
 def handle_message(data):
     sender = data['sender']
     message = data['message']
-    emit('chat message', {'sender': sender, 'message': message}, broadcast=True)
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')  # 获取当前时间
+
+    emit('chat message', {'sender': sender, 'message': message, 'timestamp': timestamp}, broadcast=True)
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=8010, debug=True, allow_unsafe_werkzeug=True )
